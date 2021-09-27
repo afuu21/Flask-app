31234cc00123eec4e383f762dda897df6d557e53
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2021-09-27 08:54:07 +0530

    Update app.py

--- Images_upload_app/app.py
+++ Images_upload_app/app.py
@@ -1,35 +1,45 @@
-from flask import Flask, render_template, request, flash, url_for
-from werkzeug.utils import redirect, secure_filename
-
+from flask import Flask, render_template, flash, request, redirect, url_for
 import urllib.request
 import os
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads/'
 
-app.secret_key = "secret key"
+app.secret_key = "cariocoders-ednalan"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-
-@app.route("/")
-def my_form():
+@app.route('/')
+def home():
     return render_template('index.html')
-
-@app.route("/", methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
-        flash('No file part')
+        flash('No file found')
         return redirect(request.url)
-    file= request.files['file']
+    file = request.files['file']
     if file.filename == '':
         flash('No image selected for uploading')
         return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Image successfully uploaded and displayed')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are png, jpg, jpeg and gif')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+if __name__ == "__main__":
+    app.run(debug=True)
